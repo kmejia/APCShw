7b1f9c260b060e45637beec0f56a37253e468403
kmejia <kmejia345@gmail.com>
2014-09-24 08:31:13 -0400

    updated for lab

--- 05-CodingBat/StringStuff.java
+++ 05-CodingBat/StringStuff.java
@@ -15,10 +15,41 @@ public class StringStuff {
 	    return Math.abs(n - 21);
 	return Math.abs((21 - n) * 2);
     }
-
+    //WORKING
 
     public String makeAbba(String a, String b) {
 	return a+b+b+a;
     }
+
+    public boolean nearHundred(int n) {
+	return (Math.abs(n - 100)<=(10)) || (Math.abs(n - 200)<=10);
+}
+
+
+    public boolean mixStart(String str) {
+	if (str.length() < 3)
+	    return false;
+	String str2 = str.substring(1, 3);
+	return(str2.equals("ix"));
+}
+
+    public int teaParty(int tea, int candy) {
+	if ((tea>=(2 * candy) || candy>=(2 *tea)) && ((tea >= 5)  && (candy >=5)))  {
+	    return 2  ;
+	}
+	if ((tea >= 5)  && (candy >=5)) {
+	    return 1;
+	} else { 
+	    return 0;
+	}
+     
+    }
+    public boolean lastDigit(int a, int b, int c) {
+	return (a % 10 == b % 10) || (a % 10 == c % 10) || (b % 10 == c % 10);
+    }
+
+
+
+
 }
   
