f93848c8cbb6d0ac75dbc53cee0c761b13dacdd6
kmejia <kmejia345@gmail.com>
2014-09-23 20:07:50 -0400

    it works

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+    public static void main(String[] args){
+	StringStuff k = new StringStuff();
+	// k is a new mamber of the class Stringstuff, it can call 
+	//any of its methods
+	//	System.out.println(nonStart("hello" , "world"));
+	System.out.println("We will now use methods nonStart, diff21, and makeAbba");
+	
+	
+	System.out.println("k.nonStart(hello , world) ==>"+k.nonStart("hello" , "world"));
+	System.out.println("k.diff21(74) ==>"+k.diff21(74));
+	System.out.println("k.makeAbba(aaaa,bbbb) ==>"+k.makeAbba("aaaa" , "bbbb"));
+    }
+}
