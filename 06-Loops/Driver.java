60911043b56d0e2043c56db96f581e3efab198ad
kmejia <kmejia345@gmail.com>
2014-09-30 20:35:22 -0400

    latest hw

---
+++ 06-Loops/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args) {
+	/*int n = 0;
+	while (n <=9   ) {
+	    System.out.println(n);
+	    n = n + 1;
+	}*/
+        Loops k = new Loops();
+	System.out.println(k.frontTimes("Chocolate",2);
+    }
+}
