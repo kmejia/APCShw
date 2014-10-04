4bc9fb40f27abc2b88777e38ae3761132beae1a9
kmejia <kmejia345@gmail.com>
2014-10-03 23:33:28 -0400

    finished

---
+++ 08-Shapes2/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args) {
+	Shapes2 k = new Shapes2();
+	System.out.println(k.frame( 5 , 4));
+	System.out.println("");
+	System.out.println(k.stringSplosion("Coding"));
+	System.out.println(k.stringX("xabxxxcdx"));
+
+    }
+}
+		   
--- 08-Shapes2/Shapes2.java
+++ 08-Shapes2/Shapes2.java
@@ -6,25 +6,52 @@ public class Shapes2 {
 	int x = n;
 	while (x > 0) {
 	    ans = ans + ch;
+	    x = x - 1;
 	}
 	return ans;
     }
 
 
-    public Sring frame(int r , int c) {
-	//int row = r;
+    public String frame(int r , int c) {
+	int row = r;
+	String ans = "";
 	int spaces = c - 2;
 	for(row = 1; row <= r; row = row + 1) {
 	    if (row==1 || row == r) {
 		ans = ans + line("*", c);
 	    }
-	    if else {
+	    else if (1 < row && row < r) {
 		ans = ans + "*" + line(" " , spaces) + "*";
 	    }
 	    ans +="\n";
-	    row = row+1;
+	    /// row = row+1; not needed
 	}
-    	   
+	return ans;
     }
+    
+    //codingbat
+    public String stringSplosion(String str) {
+	String ans = "";
+	int place;
+	for (place = 1 ; place <= str.length(); place = place + 1 ) {
+	    ans = ans + str.substring(0 , place);
+	}
+	
+	return ans;
+    }
+    
+    public String stringX(String str) {
+	String ans = "";
+	int place ;
+	for (place = 0; place < str.length(); place = place + 1) {
+	    if (place != 0 && place != str.length() && 
+		!(str.substring(place, place + 1).equals("x"))   ) {
+		ans = ans + str.substring(place, place + 1);    
+		
+	    }
+	    return ans;
+	}
+    }
+
 
 }
