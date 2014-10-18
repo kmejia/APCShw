67ea50f1c21abf8ee731f5cb30eaa6b79c34117c
kmejia <kmejia345@gmail.com>
2014-10-18 17:54:30 -0400

    l

--- Stuyablo/basechar.java
+++ Stuyablo/basechar.java
@@ -1,30 +1,30 @@
-import java.util.*;
-import java.io.*;
 public class basechar {
-    //instance variables, actual values vary for every instance of this class
     private int health;
     private int strength;
     private int defense;
-    private int speed;
+    private int speed
     private String name;
-    private int manna;
 
+    public void setSpeed (int s){
+	speed = s;
+    }
+	
     public void setHealth(int h){
-	health=h;}
+	health=h;
+    }
     public int getHealth(){
-	return health;}
+	return health;
+    }
     public void setStrength(int s){
-	strength=s;}
+	strength=s;
+    }
     public int getStrength(){
-	return strength;}
-    public void setManna(int s){
-	manna=s;}
-    public int getManna(){
-	return manna;}
-    
-    public String toString(){
-	return this.getName();
-}
+	return strength;
+    }
+    public void setName(String n){
+	name = n;
+    }
+
     public void pause(int t){
 	try {
 	    Thread.sleep(t);
--- Stuyablo/monster.java
+++ Stuyablo/monster.java
@@ -1,19 +1,9 @@
-public class monster extends basechar {
-    
-
-    public void monster() {
+public class monster extends basechar { 
+    public void monster() { 
 	setHealth(60);
 	setStrength(60);
-	
     }
-    
     public void evilAttack(basechar, other) {
-
-
 	System.out.println(this + "evilAtacked"+other);
-	
-    }
-    
-	
-
+    } 
 }
