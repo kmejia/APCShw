96bea366f4f361a4893c3ba49afdb5ac1fd8b388
kmejia <kmejia@users.noreply.github.com>
2014-10-18 20:34:25 -0400

    Delete monster.java

--- Stuyablo/monster.java
+++
@@ -1,9 +0,0 @@
-public class monster extends basechar { 
-    public void monster() { 
-	setHealth(60);
-	setStrength(60);
-    }
-    public void evilAttack(basechar, other) {
-	System.out.println(this + "evilAtacked"+other);
-    } 
-}
