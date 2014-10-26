888a56112f2f1a3e36a81cc8e76f6717ef8c1806
kmejia <kmejia345@gmail.com>
2014-10-26 00:16:59 -0400

    latest hw, newmethods discovered

---
+++ 14-Arrays/Arrays.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Arrays {
+    public int[] frontPiece(int[] nums) {
+	
+	if (nums.length > 2){
+	    return nums(0, 2);
+	} 
+	else {return nums;}
+	
+    }
+    public int sum13(int[] nums) {
+	int ans = 0;
+	
+	for ( int i=0;i<nums.length;i++) {
+	    if (nums[i]==13) {
+		if (i +2<nums.length){i+=2;}
+		else {return ans;}
+		
+	    }
+	    if (nums[i]!=13) {
+		ans = ans + nums[i];
+	    }
+	}
+	return ans;
+    }
+    
+}
---
+++ 14-Arrays/Driver.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Driver {
+    public static void main(String[] args) {
+	
+	Arrays k = new Arrays();
+	int[] numbers = {1,2,4,5,13};
+	System.out.println(k.frontPiece(numbers));
+	System.out.println(k.sum13(numbers));
+			   
+
+
+    }
+}
