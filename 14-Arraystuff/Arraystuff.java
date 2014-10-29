6f46daf4bfbbe3cfb6d9029fc3b604f53b5df98e
kmejia <kmejia345@gmail.com>
2014-10-29 02:17:24 -0400

    14b

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -27,7 +27,7 @@ public class Arraystuff {
     }
     ////hw-------------------------------------------------/-////
     public int find(int value) {
-	Arraystuff a = new Arraystuff();
+	//Arraystuff a = new Arraystuff();
 	for (int i = 0;i<a.length;i++) {
 	    if (value==a[i]){
 	    return i;
@@ -36,10 +36,10 @@ public class Arraystuff {
 	return -1;
     }
     public int MaxVal() {
-	Arraystuff a = new Arraystuff();
+	//Arraystuff a = new Arraystuff();
 
-	if (as.length==1) {return "no max";}
-	int maxs = as[0];
+	if (a.length==1) {return a[0];}
+	int maxs = a[0];
 	for (int i = 0 ;i<a.length;i++) {
 	    if (a[i] > maxs) {
 		maxs = a[i];
@@ -53,8 +53,59 @@ public class Arraystuff {
 
 
 
+    //HW14b-------------------------------------------////////////////////    
     
+
+
+    public int sum67(int[] nums) {
+	int i;
+	int ans = 0;
+	for (i = 0; i <nums.length; i++) {
+	    if (nums[i]!=6) {ans = ans + nums[i];}
+	    if (nums[i]==6) {
+		while (nums[i]!=7) {
+		    i = i +1;
+		    ans =  ans + 0;
+		}
+	    }
+	    
+	    
+	    
+	    
+	}
+	return ans;
+    }
     
+
+    public boolean more14(int[] nums) {
+	int onenum = 0;
+	int fournum = 0;
+	for (int i = 0;i<nums.length;i++) {
+	    if (nums[i]==1) {onenum+=1;}
+	    if (nums[i] ==4) {fournum+=1;} 
+	    
+	}
+	return onenum>fournum;
+	
+    }
+    
+
+
+   
+
+    
+    public boolean tripleUp(int[] nums) {
+	int[] ans = new int[3];
+	if (nums.length==3) {return (nums[0]==nums[1]-1 && nums[0]==nums[2]-2);}
+	for (int i = 0; i< nums.length-2;i++) {
+	    ans[0] = nums[i];              
+	    ans[1] = nums[i+1];
+	    
+	    ans[2] = nums[i+2];
+	    if (ans[0]==ans[1]-1 && ans[0]==ans[2]-2) {return true;}
+	}
+	return false;
+    }
     public static void main(String[] args) {
 	//this is perfectly fine, making an instance within itself
 	Arraystuff as = new Arraystuff();
